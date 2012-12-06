488a3668ffd7a4b5dd8009ab874e4da9b491c24a
Richard Hasselgren <richard.hasselgren@elev.it-gymnasiet.se>
2012-12-06 15:11:49 +0100

    first

---
+++ Monsters.rb
@@ -0,0 +1,21 @@
+class Monsters
+
+	attr_reader :name, :age, :gender: :size
+
+	def iniaitialize(name, age, gender, size)
+		@name = name.downcase.capitalize
+		@age = age
+		@gender = gender
+		@size = size
+	end
+
+	puts "Order your Monster one of theas options 
+	Sleep
+	eat"
+
+	if sleep do 0 < -1
+	elsif eat puts "yumy"
+	end
+			
+
+end
